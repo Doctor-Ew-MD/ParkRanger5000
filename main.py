71ad318cb415c4bfda69fd85c6974c687a193895
drewortego <ortegoa@gmail.com>
2026-06-16 15:01:05 -0700

    finishing touches, new token

--- main.py
+++ main.py
@@ -1,78 +1,124 @@
 import discord
 from discord.ext import commands
 
-import logging
 import os
 
+# TODO Add better, cleaner logging
 
-TOKEN = 'MTUxNDAzMzA4MjI0MjY5OTQwNA.GNzj5A.TxCJaVI1M2O7hlm87qKtJkR5LAQQJ9bY7n9R5c'  # GitHub secret
-ERROR_MSG = 'Please include a month, day, and name for your channel, such as **dec-31-nye-dance-party**'
-EVENTS_CATEGORY = 'Events'
-CHANNEL_NAME = 'event-planner'
+TOKEN = os.getenv('PARKRANGER5000_TOKEN')
+ERROR_MSG = 'Please include a month, day, and description when you create your channel, like this: **!create dec 31 nye dance party**'
+EVENTS_CATEGORY_NAME = 'Events'
+EVENTS_CHANNEL_NAME = 'event-planner'
+BOT_ROLE_NAMES = {'ParkRanger5000', 'ParkRanger'}
 
-# Set required intents for handling messages
 intents = discord.Intents.default()
-intents.message_content = True
+intents.members = True
 intents.messages = True
+intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
 
+
 @bot.command()
-async def hello(ctx):
+async def hello(ctx, *args):
+    """
+    Some helpful data to check if the bot is working properly and debug messages.
+    """
     await ctx.send('Hello!')
+
+    print(f'bot.user.id: {bot.user.id}')
+    print(f'bot.user.display_name: {bot.user.display_name}')
+    print(f'bot.user.name: {bot.user.name}')
+    print(f'bot.user.global_name: {bot.user.global_name}')
+
+    print(f'user mentions: {ctx.message.mentions}')
+    print(f'role mentions: {ctx.message.role_mentions}')
+    print(f'channel mentions: {ctx.message.channel_mentions}')
+
 
 @bot.command()
 async def create(ctx, *args):
     """
     Allow users to create a new channel, but prevent spamming and report errors.
     """
+
     if not ctx:  # This will probably never be True
         print('No context provided')
+        return
+
+    if bot.user.name in args:
+        print('bot mentioned')
         return
     
     user_name = f'{ctx.author.name}/{ctx.author.display_name}'
 
-    if ctx.message.channel.name.lower() != CHANNEL_NAME:
-        await ctx.send(f'That command only works in the **{CHANNEL_NAME}** channel!')
+    if ctx.message.channel.name.lower() != EVENTS_CHANNEL_NAME:
         print(f'Failed event-planner check from {user_name}')
+        await ctx.send(f'That command only works in the **{EVENTS_CHANNEL_NAME}** channel!')
         return
 
     if not args or len(args) < 3:
-        await ctx.send(ERROR_MSG)
         print(f'Failed args or len(args) check from {user_name}')
+        await ctx.send(f'That channel name isn\'t quite detailed enough!')
+        await ctx.send(ERROR_MSG)
         return
 
     try:
         int(args[1])
     except ValueError:
-        await ctx.send(ERROR_MSG)
         print(f'Failed int check from {user_name}')
+        await ctx.send('That channel name didn\'t meet the format requirements of month-day-title.')
+        await ctx.send(ERROR_MSG)
         return
 
     channel_name = '-'.join(args)
 
-    if existing := discord.utils.get(ctx.guild.channels, name=channel_name):
-        await ctx.send(f'A channel named **{channel_name}** already exists!')
+    if existing := discord.utils.get(ctx.guild.channels, name=channel_name):        
         print(f'Failed existing check from {user_name}')
+        await ctx.send(f'A channel named **{channel_name}** already exists!')
         return
 
-    category = discord.utils.get(ctx.guild.categories, name=EVENTS_CATEGORY)
+    if len(channel_name) > 40:
+        print(f'Channel\'s name exceeded the maximum length from {user_name}')
+        await ctx.send(f'That channel name is a little too long, could you please shorten it and try again?')
+        await ctx.send(ERROR_MSG)
+        return
+
+    category = discord.utils.get(ctx.guild.categories, name=EVENTS_CATEGORY_NAME)
 
     print(f'Attempting to create channel **{channel_name}** from {user_name}')
-    await ctx.guild.create_text_channel(
+    channel = await ctx.guild.create_text_channel(
         channel_name,
         overwrites={},
         category=category,
         reason=f'created by {user_name}',
     )
-
-    await ctx.send(f'Created channel **{channel_name}**!')
     print(f'New channel **{channel_name}** created by {user_name}')
+    await ctx.send(f'Here ya go! {channel.jump_url}')
 
-bot.run(TOKEN, log_handler=handler)
+
+@bot.event
+async def on_message(message):
+    if message.author.id == bot.user.id:
+        return
+
+    bot_mentioned = bot.user in message.mentions
+    role_mentioned = any(role.name in BOT_ROLE_NAMES for role in message.role_mentions)
+
+    if bot_mentioned or role_mentioned:
+        print('Bot was mentioned!')
+        ctx = await bot.get_context(message)
+        events_channel = discord.utils.get(ctx.guild.channels, name=EVENTS_CHANNEL_NAME)
+        await message.channel.send(f'Hi, I\'m a bot made to help you create channels in {events_channel.jump_url}.')
+        await message.channel.send(f'Head over there to create a channel, or DM an admin if you need help.')
+        await message.channel.send(ERROR_MSG)
+
+    await bot.process_commands(message)
+
+
+bot.run(TOKEN)
